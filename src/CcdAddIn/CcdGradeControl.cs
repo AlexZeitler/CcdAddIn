4e574f1cfee102f00e97d74fda7851603374b832
alexander.zeitler <alexander.zeitler@b31419d6-dfed-11dd-bce9-bf72c0239d3a>
2009-01-15 22:35:14 +0000

    Implemented parsing of config and mappings Implemented basic xslt transformations Actually not parsing a specific grade. No selection of grade.
    git-svn-id: http://ccdaddin.googlecode.com/svn/trunk@4 b31419d6-dfed-11dd-bce9-bf72c0239d3a

--- src/CcdAddIn/CcdGradeControl.cs
+++ src/CcdAddIn/CcdGradeControl.cs
@@ -1,21 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 
 namespace CcdAddIn {
     public partial class CcdGradeControl : UserControl {
         public CcdGradeControl() {
             InitializeComponent();
 
-            const string CCDXMLPATH = "file:///{0}/Desktop/test.htm";
+            string[] ccdGrades;
+            string defaultGrade = string.Empty;
+            string currentGrade = string.Empty;
+            string xmlInputPath = string.Empty;
+            string xsltInputPath = string.Empty;
+
+            Dictionary<string, string[]> gradeMappings = new Dictionary<string, string[]>();
+            Dictionary<string, string[]> gradeDefinitions = new Dictionary<string, string[]>();
+
+
+            const string VSADDINPATH = "{0}\\Documents\\Visual Studio 2008\\Addins";
+            const string XMLINPUTPATH = "{0}\\{1}";
+            const string XSLTINPUTPATH = "{0}\\{1}";
+            const string OUTPUTHTMLFILENAME = "{0}\\CcdWerteSystem.html";
+            const string OUTPUTHTMLFILEURI = "file:///{0}";
 
             string userprofilepath = Environment.GetEnvironmentVariable("userprofile");
-            string ccdGradePath = string.Format(CCDXMLPATH, userprofilepath);
-            this.gradeBrowser.Url = new Uri(ccdGradePath);
+
+            string localAddInPath = string.Format(VSADDINPATH, userprofilepath);
+            string localOutputHtmlFilePath = string.Format(OUTPUTHTMLFILENAME, localAddInPath);
+
+            string assemblyPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            string configPath = string.Format("{0}\\CcdConfig.xml", assemblyPath);
+
+            XDocument configDocument = XDocument.Load(configPath);
+            var config = from d in configDocument.Descendants("Konfiguration") select d;
+            if(config.Count() == 1) {
+                defaultGrade = config.First().Attribute("standardGrad").Value;
+                currentGrade = config.First().Attribute("aktiverGrad").Value;
+                xmlInputPath = config.First().Attribute("xmlInput").Value;
+                xsltInputPath = config.First().Attribute("xsltInput").Value;
+            }
+
+            string localXmlInputPath = string.Format(XMLINPUTPATH, localAddInPath, xmlInputPath);
+            string localXsltInputPath = string.Format(XSLTINPUTPATH, localAddInPath, xsltInputPath);
+
+            var mappings = from d in config.Descendants("Grad") select d;
+            foreach (XElement el in mappings) {
+                gradeMappings.Add(el.Attribute("farbe").Value, new[] { el.Attribute("rgb").Value});
+            }
+
+
+            try {
+                XsltArgumentList xslArg = new XsltArgumentList();
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                xslt.Load(localXsltInputPath);
+                XPathDocument xpathdocument = new XPathDocument(localXmlInputPath);
+                XmlTextWriter writer = new XmlTextWriter(localOutputHtmlFilePath, null);
+                //xslt.Transform(xpathdocument, xslArg, writer);
+                xslt.Transform(xpathdocument, writer);
+
+                writer.Close();
+
+                string localHtmlPath = string.Format(OUTPUTHTMLFILEURI, localOutputHtmlFilePath);
+                gradeBrowser.Url = new Uri(localHtmlPath);
+
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+
         }
     }
 }
--- src/CcdAddIn/CcdWerteSystem.xslt
+++ src/CcdAddIn/CcdWerteSystem.xslt
@@ -2,11 +2,43 @@
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
 >
-    <xsl:output method="xml" indent="yes"/>
 
-    <xsl:template match="@* | node()">
-        <xsl:copy>
-            <xsl:apply-templates select="@* | node()"/>
-        </xsl:copy>
-    </xsl:template>
+    <xsl:output method="xml" indent="yes"/>
+    <xsl:template match="/">
+
+		<html>
+			<body>
+				<table border="0">
+					<xsl:for-each select="CCDWerteSystem/Grad">
+						<tr>
+							<td>
+								<xsl:value-of select="@farbe"/>
+
+								<h4>Prinzipien</h4>
+								<ul>
+									<xsl:for-each select="Prinzipien/Prinzip">
+										<li>
+											<a href="{@href}" target="_blank"><xsl:value-of select="@titel"/></a>
+										</li>
+									</xsl:for-each>
+								</ul>
+								<h4>Regeln</h4>
+								<xsl:for-each select="Regeln/Regel">
+									<xsl:value-of select="@titel"/>
+									<br/>
+								</xsl:for-each>
+								<h4>Praktiken</h4>
+								<xsl:for-each select="Praktiken/Praktik">
+									<xsl:value-of select="@titel"/>
+									<br/>
+								</xsl:for-each>
+							</td>
+						</tr>
+					</xsl:for-each>
+				</table>
+			</body>
+		</html>
+
+
+	</xsl:template>
 </xsl:stylesheet>
