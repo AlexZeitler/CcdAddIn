7709daa3f1f1537f8d7c4368e1022a5d790ff9da
alexander.zeitler <alexander.zeitler@b31419d6-dfed-11dd-bce9-bf72c0239d3a>
2009-01-17 21:33:12 +0000

    Completed XSLT parsing Added missing grades to CcdWerteSystem.xml Reading "My Documents" path from Registry
    git-svn-id: http://ccdaddin.googlecode.com/svn/trunk@5 b31419d6-dfed-11dd-bce9-bf72c0239d3a

--- src/CcdAddIn/CcdGradeControl.cs
+++ src/CcdAddIn/CcdGradeControl.cs
@@ -11,6 +11,7 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using System.Xml.Xsl;
+using Microsoft.Win32;
 
 namespace CcdAddIn {
     public partial class CcdGradeControl : UserControl {
@@ -27,15 +28,22 @@ namespace CcdAddIn {
             Dictionary<string, string[]> gradeDefinitions = new Dictionary<string, string[]>();
 
 
-            const string VSADDINPATH = "{0}\\Documents\\Visual Studio 2008\\Addins";
+            const string VSADDINPATH = "{0}\\Visual Studio 2008\\Addins";
             const string XMLINPUTPATH = "{0}\\{1}";
             const string XSLTINPUTPATH = "{0}\\{1}";
             const string OUTPUTHTMLFILENAME = "{0}\\CcdWerteSystem.html";
             const string OUTPUTHTMLFILEURI = "file:///{0}";
 
-            string userprofilepath = Environment.GetEnvironmentVariable("userprofile");
 
-            string localAddInPath = string.Format(VSADDINPATH, userprofilepath);
+            RegistryKey key =
+                Registry.CurrentUser.OpenSubKey(
+                    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders");
+
+            string userDocumentsPath = key.GetValue("Personal").ToString();
+
+            //string userDocumentsPath = Environment.GetEnvironmentVariable("userprofile");
+
+            string localAddInPath = string.Format(VSADDINPATH, userDocumentsPath);
             string localOutputHtmlFilePath = string.Format(OUTPUTHTMLFILENAME, localAddInPath);
 
             string assemblyPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
@@ -43,7 +51,7 @@ namespace CcdAddIn {
 
             XDocument configDocument = XDocument.Load(configPath);
             var config = from d in configDocument.Descendants("Konfiguration") select d;
-            if(config.Count() == 1) {
+            if (config.Count() == 1) {
                 defaultGrade = config.First().Attribute("standardGrad").Value;
                 currentGrade = config.First().Attribute("aktiverGrad").Value;
                 xmlInputPath = config.First().Attribute("xmlInput").Value;
@@ -55,7 +63,7 @@ namespace CcdAddIn {
 
             var mappings = from d in config.Descendants("Grad") select d;
             foreach (XElement el in mappings) {
-                gradeMappings.Add(el.Attribute("farbe").Value, new[] { el.Attribute("rgb").Value});
+                gradeMappings.Add(el.Attribute("farbe").Value, new[] { el.Attribute("rgb").Value });
             }
 
 
@@ -64,9 +72,14 @@ namespace CcdAddIn {
                 XslCompiledTransform xslt = new XslCompiledTransform();
                 xslt.Load(localXsltInputPath);
                 XPathDocument xpathdocument = new XPathDocument(localXmlInputPath);
-                XmlTextWriter writer = new XmlTextWriter(localOutputHtmlFilePath, null);
+                XmlTextWriter writer = new XmlTextWriter(localOutputHtmlFilePath, Encoding.UTF8);
+
+                XsltArgumentList xsltArgumentList = new XsltArgumentList();
+                xsltArgumentList.AddParam("Grad", string.Empty, currentGrade);
+                xsltArgumentList.AddParam("RGB", string.Empty, gradeMappings[currentGrade][0]);
+
                 //xslt.Transform(xpathdocument, xslArg, writer);
-                xslt.Transform(xpathdocument, writer);
+                xslt.Transform(xpathdocument, xsltArgumentList, writer);
 
                 writer.Close();
 
--- src/CcdAddIn/CcdWerteSystem.xslt
+++ src/CcdAddIn/CcdWerteSystem.xslt
@@ -1,41 +1,87 @@
 ﻿<?xml version="1.0" encoding="utf-8"?>
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-    xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl"
->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:msxsl="urn:schemas-microsoft-com:xslt" exclude-result-prefixes="msxsl">
 
-    <xsl:output method="xml" indent="yes"/>
-    <xsl:template match="/">
+	<xsl:output method="text" indent="yes" />
+	<xsl:param name="Grad"></xsl:param>
+	<xsl:param name="RGB"></xsl:param>
+	<xsl:template match="/">
 
 		<html>
-			<body>
-				<table border="0">
-					<xsl:for-each select="CCDWerteSystem/Grad">
-						<tr>
-							<td>
-								<xsl:value-of select="@farbe"/>
 
-								<h4>Prinzipien</h4>
-								<ul>
-									<xsl:for-each select="Prinzipien/Prinzip">
-										<li>
-											<a href="{@href}" target="_blank"><xsl:value-of select="@titel"/></a>
-										</li>
-									</xsl:for-each>
-								</ul>
-								<h4>Regeln</h4>
-								<xsl:for-each select="Regeln/Regel">
-									<xsl:value-of select="@titel"/>
-									<br/>
-								</xsl:for-each>
-								<h4>Praktiken</h4>
-								<xsl:for-each select="Praktiken/Praktik">
-									<xsl:value-of select="@titel"/>
-									<br/>
-								</xsl:for-each>
-							</td>
-						</tr>
-					</xsl:for-each>
-				</table>
+			<head>
+
+				<style type="text/css">
+					body {
+					font-size:0.8em;
+					font-family: Arial;
+					}
+
+					h1, h2 {
+					font-size:0.9em;
+					}
+
+					.currentGrade {
+					width:100%;
+					height:20px;
+					background-color:#<xsl:value-of select="$RGB"/>;
+					}
+				</style>
+
+			</head>
+
+
+			<body>
+				<xsl:for-each select="CCDWerteSystem/Grad[@farbe=$Grad]">
+
+					<h1>
+						Aktueller Grad: <xsl:value-of select="@farbe"/>
+					</h1>
+
+					<div class="currentGrade">
+						&#160;
+					</div>
+
+					<xsl:if test="count(Prinzipien/Prinzip) &gt; 0">
+						<h2>Prinzipien</h2>
+						<ul>
+							<xsl:for-each select="Prinzipien/Prinzip">
+								<li>
+									<a href="{@href}" target="_blank">
+										<xsl:value-of select="@titel"/>
+									</a>
+								</li>
+							</xsl:for-each>
+						</ul>
+					</xsl:if>	
+						
+					<xsl:if test="count(Regeln/Regel) &gt; 0">
+						<h2>Regeln</h2>
+						<ul>
+							<xsl:for-each select="Regeln/Regel">
+								<li>
+									<a href="{@href}" target="_blank">
+										<xsl:value-of select="@titel"/>
+									</a>
+								</li>
+							</xsl:for-each>
+						</ul>
+					</xsl:if>
+
+					<xsl:if test="count(Praktiken/Praktik) &gt; 0">
+						<h2>Praktiken</h2>
+						<ul>
+							<xsl:for-each select="Praktiken/Praktik">
+								<li>
+									<a href="{@href}" target="_blank">
+										<xsl:value-of select="@titel"/>
+									</a>
+								</li>
+							</xsl:for-each>
+						</ul>
+					</xsl:if>	
+						
+				</xsl:for-each>
+
 			</body>
 		</html>
 
