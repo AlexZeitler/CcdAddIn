993e4775aac320c3e3efd212559cfe5b0bde7163
slove@matas.de <slove@matas.de@b31419d6-dfed-11dd-bce9-bf72c0239d3a>
2009-08-17 13:21:58 +0000

    fixed http://code.google.com/p/ccdaddin/issues/detail?id=6
    git-svn-id: http://ccdaddin.googlecode.com/svn/trunk@17 b31419d6-dfed-11dd-bce9-bf72c0239d3a

--- src/CcdAddIn/CcdGradeControl.cs
+++ src/CcdAddIn/CcdGradeControl.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,91 +7,102 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using System.Xml.Xsl;
-using Microsoft.Win32;
 
-namespace CcdAddIn {
-    public partial class CcdGradeControl : UserControl {
-        public CcdGradeControl() {
-            InitializeComponent();
+namespace CcdAddIn
+{
+    public partial class CcdGradeControl : UserControl
+    {
+        public CcdGradeControl()
+        {
+            try
+            {
+                #region Initialisierung
+                InitializeComponent();
 
-            string[] ccdGrades;
-            string defaultGrade = string.Empty;
-            string currentGrade = string.Empty;
-            string xmlInputPath = string.Empty;
-            string xsltInputPath = string.Empty;
+                string defaultGrade = string.Empty;
+                string currentGrade = string.Empty;
+                string xmlInputPath = string.Empty;
+                string xsltInputPath = string.Empty;
 
-            Dictionary<string, string[]> gradeMappings = new Dictionary<string, string[]>();
-            Dictionary<string, string[]> gradeDefinitions = new Dictionary<string, string[]>();
+                Dictionary<string, string[]> gradeMappings = new Dictionary<string, string[]>();
+                Dictionary<string, string[]> gradeDefinitions = new Dictionary<string, string[]>();
 
+                // den pfad merken, von dem das _aktuelle_ AddIn ( also "ICH" ) geldaen wurde
+                string VSADDINPATH = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
 
-            const string VSADDINPATH = "{0}\\Visual Studio 2008\\Addins";
-            const string XMLINPUTPATH = "{0}\\{1}";
-            const string XSLTINPUTPATH = "{0}\\{1}";
-            const string OUTPUTHTMLFILENAME = "{0}\\CcdWerteSystem.html";
-            const string OUTPUTHTMLFILEURI = "file:///{0}";
+                // eigentlich kommt _nur_ der XmlTextWriter mit Uri-Pfaden nicht klar
+                // sei's drum, sieht auch "huebscher" aus im Debug-Log ;)
+                VSADDINPATH = CcdAddInUtil.NormalizePath(VSADDINPATH);
 
+                const string XMLINPUTPATH = "{0}\\{1}";
+                const string XSLTINPUTPATH = "{0}\\{1}";
+                const string OUTPUTHTMLFILENAME = "{0}\\CcdWerteSystem.html";
+                const string OUTPUTHTMLFILEURI = "file:///{0}";
+                string localOutputHtmlFilePath = string.Format(OUTPUTHTMLFILENAME, VSADDINPATH);
+                #endregion
 
-            RegistryKey key =
-                Registry.CurrentUser.OpenSubKey(
-                    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders");
+                #region konfigurationsdatei laden und werte auslesen
+                string configPath = string.Format("{0}\\CcdConfig.xml", VSADDINPATH);
+                CcdAddInDebug.WriteLine(string.Format("CcdAddIn :: Load '{0} ...'", configPath));
+                XDocument configDocument = XDocument.Load(configPath);
+                var config = from d in configDocument.Descendants("Konfiguration") select d;
+                if (config.Count() == 1)
+                {
+                    defaultGrade = config.First().Attribute("standardGrad").Value;
+                    currentGrade = config.First().Attribute("aktiverGrad").Value;
+                    xmlInputPath = config.First().Attribute("xmlInput").Value;
+                    xsltInputPath = config.First().Attribute("xsltInput").Value;
+                }
+                #endregion
 
-            string userDocumentsPath = key.GetValue("Personal").ToString();
+                #region einlesen der datenbasis aus XmlInput datei
+                string localXmlInputPath = string.Format(XMLINPUTPATH, VSADDINPATH, xmlInputPath);
+                string localXsltInputPath = string.Format(XSLTINPUTPATH, VSADDINPATH, xsltInputPath);
+                CcdAddInDebug.WriteLine(string.Format("CcdAddIn :: Load '{0} ...'", localXmlInputPath));
+                XDocument xmlInput = XDocument.Load(localXmlInputPath);
+                var grades = from g in xmlInput.Descendants("Grad")
+                             where (string)g.Attribute("farbe").Value == currentGrade
+                             select g;
 
-            //string userDocumentsPath = Environment.GetEnvironmentVariable("userprofile");
+                string rgb = string.Empty;
+                if (grades.Count() == 1)
+                {
+                    rgb = grades.First().Attribute("RGB").Value;
+                }
+                #endregion
 
-            string localAddInPath = string.Format(VSADDINPATH, userDocumentsPath);
-            string localOutputHtmlFilePath = string.Format(OUTPUTHTMLFILENAME, localAddInPath);
-
-            string assemblyPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
-            string configPath = string.Format("{0}\\CcdConfig.xml", assemblyPath);
-
-            XDocument configDocument = XDocument.Load(configPath);
-            var config = from d in configDocument.Descendants("Konfiguration") select d;
-            if (config.Count() == 1) {
-                defaultGrade = config.First().Attribute("standardGrad").Value;
-                currentGrade = config.First().Attribute("aktiverGrad").Value;
-                xmlInputPath = config.First().Attribute("xmlInput").Value;
-                xsltInputPath = config.First().Attribute("xsltInput").Value;
-            }
-
-            string localXmlInputPath = string.Format(XMLINPUTPATH, localAddInPath, xmlInputPath);
-            string localXsltInputPath = string.Format(XSLTINPUTPATH, localAddInPath, xsltInputPath);
-
-            XDocument xmlInput = XDocument.Load(localXmlInputPath);
-            var grades = from g in xmlInput.Descendants("Grad") 
-                         where (string)g.Attribute("farbe").Value == currentGrade 
-                         select g;
-
-            string rgb = string.Empty;
-            if(grades.Count() == 1) {
-                rgb = grades.First().Attribute("RGB").Value;
-            }
-
-            try {
+                #region XmlInput datei per XSLT-Transformation in ein HTML-Dokument umwandeln
                 XsltArgumentList xslArg = new XsltArgumentList();
                 XslCompiledTransform xslt = new XslCompiledTransform();
-                xslt.Load(localXsltInputPath);
-                XPathDocument xpathdocument = new XPathDocument(localXmlInputPath);
-                XmlTextWriter writer = new XmlTextWriter(localOutputHtmlFilePath, Encoding.UTF8);
 
+                CcdAddInDebug.WriteLine(string.Format("CcdAddIn :: Load '{0} ...'", localXsltInputPath));
+                xslt.Load(localXsltInputPath);
+
+                XPathDocument xpathdocument = new XPathDocument(localXmlInputPath);
+
+                CcdAddInDebug.WriteLine(string.Format("CcdAddIn :: Create '{0} ...'", localOutputHtmlFilePath));
+
+                XmlTextWriter writer = new XmlTextWriter(localOutputHtmlFilePath, Encoding.UTF8);
                 XsltArgumentList xsltArgumentList = new XsltArgumentList();
                 xsltArgumentList.AddParam("AktuellerGrad", string.Empty, currentGrade);
                 xsltArgumentList.AddParam("RGB", string.Empty, rgb);
-
-                //xslt.Transform(xpathdocument, xslArg, writer);
                 xslt.Transform(xpathdocument, xsltArgumentList, writer);
-
                 writer.Close();
+                #endregion
 
+                #region generiertes HTML-Dokument im Browser-Control anzeigen
                 string localHtmlPath = string.Format(OUTPUTHTMLFILEURI, localOutputHtmlFilePath);
+                CcdAddInDebug.WriteLine(string.Format("CcdAddIn :: Load '{0} ...'", localHtmlPath));
                 gradeBrowser.Url = new Uri(localHtmlPath);
+                #endregion
 
             }
-            catch (Exception e) {
-                Console.WriteLine(e);
+            catch (Exception e)
+            {
+                CcdAddInDebug.WriteLine(e);
             }
-
-
         }
-    }
-}
+
+    } // class
+
+} // namespace
